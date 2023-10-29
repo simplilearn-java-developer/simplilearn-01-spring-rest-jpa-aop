502d1d250e7eea08d42ee88ddecb80372e242676
Carlos Zegarra <cgzegarra@gmail.com>
2023-10-29 12:06:04 -0400

    first commit

---
+++ src/main/java/com/simplilearn/spring/Simplilearn01SpringJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simplilearn01SpringJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Simplilearn01SpringJdbcApplication.class, args);
+	}
+
+}
