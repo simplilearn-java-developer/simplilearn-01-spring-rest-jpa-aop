df018e1e08af18b6f508dcdacd8a2d3590dd4860
Carlos Zegarra <cgzegarra@gmail.com>
2023-10-29 13:41:32 -0400

    Add Logger

--- src/main/java/com/simplilearn/spring/controller/UserController.java
+++ src/main/java/com/simplilearn/spring/controller/UserController.java
@@ -2,6 +2,8 @@ package com.simplilearn.spring.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +15,24 @@ import com.simplilearn.spring.service.UserService;
 @Controller
 public class UserController {
 
+	final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	UserService userService;
 	
     @GetMapping("/")
     String showIndex() {
 
+    	logger.debug("Showing Index...");
+    	
         return "index";
     }
     
     @GetMapping("/list")
     ModelAndView list() {
     	
+    	logger.debug("Listing Users...");
+
     	List<User> users = this.userService.list();
     	
     	return new ModelAndView("list","users",users);
